ebb9413a769d8c10cb5f8c56583620ec1d6f8ac1
Scott Lee <glaze@whyi.net>
2023-11-15 05:00:47 -0800

    Added test cases

--- __tests__/unit/DelaunayTriangulation.test.ts
+++ __tests__/unit/DelaunayTriangulation.test.ts
@@ -1,4 +1,32 @@
+/**
+ * @jest-environment jsdom
+ */
 
-describe('sum module', () => {
-  test('adds 1 + 2 to equal 3', () => {
-    expect(4).toBe(4);  });});
+import {expect} from '@jest/globals';
+import p5 from "p5";
+import GeometricOperations from '../../src/GeometricOperations';
+
+describe('DelaunayTriangulation', () => {
+  describe("addPoint", () => {
+    it ("creates a new point and add it to gemoetry table", () => {
+
+    })
+  })
+
+  describe("isInTriangle", () => {
+    it ("returns true when given point is in triangle", () => {
+
+    })
+
+    it ("returns false when given point is not in triangle", () => {
+
+    })
+  })
+
+  describe("fixMesh", () => {
+    it ("recursively flipCorners", () => {
+
+    })
+  })
+
+});
