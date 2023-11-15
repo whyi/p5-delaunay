8abd12860b2ea3912d49bba6514f0d8ffd301480
Scott Lee <glaze@whyi.net>
2023-11-15 02:25:00 -0800

    Added Mesh2D.test.ts placeholder

---
+++ __tests__/unit/Mesh2D.test.ts
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {expect, jest, test} from '@jest/globals';
+import p5, { Vector } from "p5";
+
+describe('Mesh2D', () => {
+    describe("foo", () => {
+        it ("bar", () => {
+
+        })
+    })
+});
