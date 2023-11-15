22dcdd48e51f2d18a31f454e3209e31aca22d2ee
Scott Lee <glaze@whyi.net>
2023-11-15 00:19:05 -0800

    Add cross product test

---
+++ __tests__/unit/GeometricOperations.test.ts
@@ -0,0 +1,14 @@
+/**
+ * @jest-environment jsdom
+ */
+
+import {expect, jest, test} from '@jest/globals';
+import p5 from "p5";
+
+describe('GeometricOperations', () => {
+    describe("Cross product", () => {
+        it ("should compute cross product only of 2D components", () => {
+            
+        })
+    })
+});
