cb80730e55c3297c822274009be20fe10251f117
Scott Lee <glaze@whyi.net>
2023-11-15 05:32:20 -0800

    added addPoint implementation

--- src/DelaunayTriangulation.ts
+++ src/DelaunayTriangulation.ts
@@ -1,5 +1,10 @@
+import p5 from "p5";
 import Mesh2D from "./Mesh2D";
 
 export default class DelaunayTriangulation extends Mesh2D {
-
+    public addPoint(x:number, y:number): void {
+        const newPoint = new p5.Vector(x, y);
+        this.vertices.push(newPoint);
+        ++this.numberOfVertices;
+    }
 }
