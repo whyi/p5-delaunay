f96f976060652af4e9b04e0ceee576fdea44688c
Scott Lee <glaze@whyi.net>
2023-11-15 05:04:30 -0800

    Added InitTwoTriangles

--- __tests__/unit/DelaunayTriangulation.test.ts
+++ __tests__/unit/DelaunayTriangulation.test.ts
@@ -4,7 +4,36 @@
 
 import {expect} from '@jest/globals';
 import p5 from "p5";
-import GeometricOperations from '../../src/GeometricOperations';
+import DelaunayTriangulation from '../../src/DelaunayTriangulation';
+
+let twoTriangles: DelaunayTriangulation;
+function InitTwoTriangles()
+{
+    /*
+    0--1
+    |\ |
+    | \|
+    3--2
+    */
+    twoTriangles = new DelaunayTriangulation()
+    twoTriangles.vertices = [
+        new p5.Vector(0,0),
+        new p5.Vector(0,1),
+        new p5.Vector(1,1),
+        new p5.Vector(1,0)
+    ];
+    twoTriangles.numberOfVertices = 4;
+    twoTriangles.corners = [0,1,2,2,3,0];
+    twoTriangles.numberOfTriangles = 2;
+    twoTriangles.numberOfCorners = 6;
+}
+beforeEach(() => {
+    InitTwoTriangles();
+});
+
+afterEach(() => {
+    InitTwoTriangles();
+})
 
 describe('DelaunayTriangulation', () => {
   describe("addPoint", () => {
