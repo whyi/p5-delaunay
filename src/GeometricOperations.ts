2d2d94651206de3e8b8d861aee35fae7af65974d
Scott Lee <glaze@whyi.net>
2023-11-15 00:58:16 -0800

    implemented isLeftTurn

--- src/GeometricOperations.ts
+++ src/GeometricOperations.ts
@@ -6,6 +6,9 @@ export abstract class GeometricOperations {
     }
 
     public static isLeftTurn(A: P5.Vector, B: P5.Vector, C: P5.Vector): boolean {
-        return false;
+        const v1 = new P5.Vector(B.x-A.x, B.y-A.y, 0);
+        const v2 = new P5.Vector(C.x-B.x, C.y-B.y, 0);
+    
+        return GeometricOperations.cross2D(v1, v2) > 0;
     }
 }
